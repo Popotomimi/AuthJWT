26fc22696cb8394eeaca23ae1f546666d56f7367
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-03-25 17:46:07 -0300

    Update
    Update edit user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -5,7 +5,34 @@ import {
   IsEmail,
   IsString,
   Matches,
+  IsOptional,
+  ValidateNested,
 } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class AddressDto {
+  @IsString()
+  @IsNotEmpty({ message: 'A rua é obrigatória' })
+  @MaxLength(100, { message: 'A rua deve ter no máximo 100 caracteres' })
+  street: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'A cidade é obrigatória' })
+  @MaxLength(100, { message: 'A cidade deve ter no máximo 100 caracteres' })
+  city: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'O estado é obrigatório' })
+  @MaxLength(100, { message: 'O estado deve ter no máximo 100 caracteres' })
+  state: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(20, {
+    message: 'O código postal deve ter no máximo 20 caracteres',
+  })
+  postalCode?: string;
+}
 
 export class CreateUserDto {
   @IsEmail({}, { message: 'O e-mail deve ser válido' })
@@ -26,4 +53,21 @@ export class CreateUserDto {
   @MinLength(3, { message: 'O nome deve ter no mínimo 3 caracteres' })
   @MaxLength(100, { message: 'O nome deve ter no máximo 100 caracteres' })
   name: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(10, { message: 'O campo gênero deve ter no máximo 10 caracteres' })
+  gender?: string;
+
+  @ValidateNested()
+  @Type(() => AddressDto)
+  @IsOptional()
+  address?: AddressDto;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(20, {
+    message: 'O número de telefone deve ter no máximo 20 caracteres',
+  })
+  phone?: string;
 }
--- src/users/schema/user.schema.ts
+++ src/users/schema/user.schema.ts
@@ -1,13 +1,31 @@
 import { Schema, Document } from 'mongoose';
 
+export type Address = {
+  street?: string;
+  city?: string;
+  state?: string;
+  postalCode?: string;
+};
+
 export type UserDocument = Document & {
   email: string;
   password: string;
   name: string;
+  gender?: string;
+  address?: Address;
+  phone?: string;
 };
 
 export const UserSchema = new Schema<UserDocument>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 8, maxlength: 100 },
   name: { type: String, required: true },
+  gender: { type: String, required: false },
+  address: {
+    street: { type: String, required: false },
+    city: { type: String, required: false },
+    state: { type: String, required: false },
+    postalCode: { type: String, required: false },
+  },
+  phone: { type: String, required: false },
 });
