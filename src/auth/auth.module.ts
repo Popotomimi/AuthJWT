1a047e32db909e8ec4b3d85be8e0fe758c616386
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-08-10 22:04:29 -0300

    Add GithubStrategy at auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { GoogleStrategy } from './google.strategy';
+import { GithubStrategy } from './github.strategy';
 
 @Module({
   imports: [PassportModule, UsersModule],
   controllers: [AuthController],
-  providers: [GoogleStrategy],
+  providers: [GoogleStrategy, GithubStrategy],
 })
 export class AuthModule {}
